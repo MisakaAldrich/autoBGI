177ee9718316b25b43abda91bff7b878d7edd126
起个名字好难 <25520958+MisakaAldrich@users.noreply.github.com>
2025-06-27 09:55:49 +0800

    feat: 配置组合计

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 	"github.com/gin-contrib/gzip"
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
+	"github.com/thinkerou/favicon"
 	"html/template"
 	"io"
 	"log"
@@ -144,6 +145,7 @@ func main() {
 
 	ginServer.SetTrustedProxies(nil)
 	ginServer.Use(gzip.Gzip(gzip.DefaultCompression))
+	ginServer.Use(favicon.New("./favicon.ico"))
 
 	////加载templates中所有模板文件, 使用不同目录下名称相同的模板,注意:一定要放在配置路由之前才得行
 	//ginServer.LoadHTMLGlob("html/*")
@@ -628,9 +630,9 @@ func main() {
 	//读取statuc文件夹所有的图片
 	ginServer.GET("/images", func(context *gin.Context) {
 
-		autoLog.Sugar.Infof("当前目录:%s", Config.BasePath)
+		imageDir := Config.BasePath + "/static/image"
 
-		imageDir := filepath.Join(Config.BasePath, "static/image")
+		autoLog.Sugar.Infof("当前目录:%s", imageDir)
 
 		files, err := os.ReadDir(imageDir)
 		if err != nil {
@@ -660,6 +662,8 @@ func main() {
 
 	//检查BGI状态
 	go bgiStatus.CheckBetterGIStatus()
+	//
+	//config.GetGachaLog(1)
 
 	if Config.IsMysSignIn {
 		//米游社自动签到
